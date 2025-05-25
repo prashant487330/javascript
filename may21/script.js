f9e6ddec2b5aa256a99c1998d2a83133fe1286ae
prashant patel <prashantpatel487330@gmail.com>
2025-05-25 10:30:04 +0530

    new

---
+++ may21/script.js
@@ -0,0 +1,64 @@
+// let x;
+// x = Math.PI;
+
+// x = Math.sqrt(25);
+
+// // returns 1 if num is negative , 1 if num is positive
+
+// x = Math.sign(675);
+
+// // x to the power of y(x,y)
+// x = Math.pow(2,3);
+
+// // after .5 round off to upper value
+// x = Math.round(45.56);
+
+
+// // round off to upper value
+// // x = Math.ceil(3.1);
+
+
+// // returns only the intiger value
+// x = Math.trunc(45.64565);
+
+// // returns the positive value
+// x = Math.abs(-345);
+
+// // return the min value
+// x = Math.min(45,353,373,5989,-56,-383,-567);
+
+// // returns the max value
+// x = Math.max(46467,2898873,283883,28282377);
+
+// // returns the random number b/w 0 to 1
+// x = Math.random();
+
+// // return the random number b/w 0 to 9
+// x = Math.floor(Math.random()*10);
+
+// // return a random number b/w 1 to 100
+// x = Math.floor(Math.random()*101) + 1;
+// console.log(x);
+
+
+// task
+let x;
+x = Math.floor(Math.random()*101) + 1;
+
+let y;
+y = Math.floor(Math.random()*51) + 1;
+
+let sum = x+y;
+console.log("sum:"+sum);
+
+let difference = x-y;
+console.log("difference:"+difference);
+
+let product = x*y;
+console.log("product:"+product);
+
+let remainder = x%y;
+console.log("remainder:"+remainder);
+
+let quotient = x/y;
+console.log("quotient:"+quotient);
