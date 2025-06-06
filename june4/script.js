167e8af4ebb46434a758e1645cbcdfb0688f10b5
prashant patel <prashantpatel487330@gmail.com>
2025-06-06 10:10:24 +0530

    new file

---
+++ june4/script.js
@@ -0,0 +1,29 @@
+// let ans=window.confirm("Did you like my website");
+// console.log(ans);
+
+
+// // reverse number
+// let num=1234;
+// console.log(Number(num.toString().split("").reverse().join("")));
+
+// let fun1=()=>{
+//     alert("i am from fun 1");
+// }
+
+// let fun2=()=>{
+//     alert("i am from fun 2");
+// }
+
+
+// DOM Methods-
+let hello=()=>{
+    // document.getElementById("ID name")
+    // document.getElementsByClassName("class name")
+    let ptag=document.querySelector("#text")
+
+    ptag.style.color="green"  
+    ptag.style.fontSize="50px"  
+    // ptag.innerHTML="<h1>HELLO</h>"
+   // ptag.innerText
+   //ptag.textContent
+}
