2ee02a5932108a991509853cdb5678cc4712e115
prashant patel <prashantpatel487330@gmail.com>
2025-06-07 15:47:37 +0530

    new

---
+++ june5/dom.js
@@ -0,0 +1,8 @@
+let a=document.getElementById("head")
+a.textContent="Prashant"
+
+let b=document.getElementById("para")
+b.innerHTML="<u>Patel</u>"
+
+let c=document.getElementById("dom")
+c.innerText="from js"
