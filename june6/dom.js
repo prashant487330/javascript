6bc096a24f152966be3764b25ac33449507b0c17
prashant patel <prashantpatel487330@gmail.com>
2025-06-09 07:33:03 +0530

    new

---
+++ june6/dom.js
@@ -0,0 +1,12 @@
+let hello=()=>{
+    let ok=document.querySelector("#ok")
+    ok.style.backgroundColor="red"
+}
+
+
+
+let hii=()=>{
+    let ok=document.querySelector("#ok")
+    // ok.style.backgroundColor="green"
+    ok.style.backgroundImage="url(img.jpg)"
+}
---
+++ june6/task.js
@@ -0,0 +1,14 @@
+let hello=()=>{
+    let cont=document.querySelector("#cont")
+    cont.style.backgroundImage="url(main.jpeg)"
+}
+
+let hii=()=>{
+    let cont=document.querySelector("#cont")
+    cont.style.backgroundImage="url(one.jpeg)"
+}
+
+let byy=()=>{
+    let cont=document.querySelector("#cont")
+    cont.style.backgroundImage="url(three.jpeg)"
+}
